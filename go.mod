2c9a269abf5882fd7bb31c5b849ad70242e7a020
Serhii Hromov <melifarowow@gmail.com>
2022-05-03 20:15:19 +0300

    public

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/hromov/muser
+
+go 1.18
+
+require github.com/harlow/authtoken v0.0.0-20151218063325-ef4b3666db99
--- muser.go
+++ muser.go
@@ -7,18 +7,11 @@ import (
 	"log"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/harlow/authtoken"
 )
 
 const accesPath = "https://www.googleapis.com/oauth2/v3/tokeninfo?access_token="
-
-var adminEmails []string
-
-func init() {
-	adminEmails = append(adminEmails, "melifarowow@gmail.com")
-}
 
 type googleAuthResponse struct {
 	Aud           string
@@ -66,49 +59,4 @@ func GetMailByToken(r *http.Request) (string, error) {
 		return answer.Email, nil
 	}
 	return "", errors.New("Token expired")
-}
-
-// IsAdmin - check if admin
-func IsAdmin(r *http.Request) (bool, error) {
-	// ctx := r.Context()
-	//client := urlfetch.Client(ctx)
-	token, err := authtoken.FromRequest(r)
-	if err != nil {
-		log.Printf("token from request error: %v", err)
-		return false, err
-	}
-	resp, err := http.Get(accesPath + token)
-	if err != nil {
-		log.Printf("token check error: %v", err)
-		return false, err
-	}
-
-	body, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Printf("ioutil.ReadAll error: %v", err)
-		return false, err
-	}
-
-	answer := new(googleAuthResponse)
-	err = json.Unmarshal(body, answer)
-	if err != nil {
-		log.Printf("json.Unmarashal error: %v", err)
-		return false, err
-	}
-
-	expires, err := strconv.Atoi(answer.ExpiresIn)
-	if err != nil {
-		log.Printf("strconv.Atoi(expires) error: %v", err)
-		return false, err
-	}
-	if expires > 0 {
-		for _, email := range adminEmails {
-			if strings.Compare(email, answer.Email) == 0 {
-				return true, nil
-			}
-		}
-		return false, nil
-	}
-	return false, errors.New("Token expired")
 }
